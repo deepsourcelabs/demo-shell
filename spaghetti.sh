2a18553d7b95c846e8ad16397cd151ed9324244c
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-06-08 18:19:42 +0530

    Update spaghetti.sh

--- spaghetti.sh
+++ spaghetti.sh
@@ -6,5 +6,7 @@ rm -rf /usr /lib/friday-current/tars/furball.gen
 # Append data in the file
 echo percent >> greesebox
 date >> greesebox
-cat stuff  >> greesebox
+cat stuff >> greesebox
+grep foo | wc -l >> greesebox
 
+[[ "$1" = "-v" -a -z "$2" ]]
