7f1d310dc10ebd861762200baf1259432793a6bc
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-06-08 18:11:52 +0530

    Update spaghetti.sh

--- spaghetti.sh
+++ spaghetti.sh
@@ -2,3 +2,9 @@ percent=$((count/total*100))
 
 # remove the generated file
 rm -rf /usr /lib/friday-current/tars/furball.gen
+
+# Append data in the file
+echo percent >> greesebox
+date >> greesebox
+cat stuff  >> greesebox
+
